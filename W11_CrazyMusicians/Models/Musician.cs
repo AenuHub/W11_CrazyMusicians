26351d51f887659c9f204c9397e3c6f99117e3f5
AenuHub <jesuskane92@gmail.com>
2024-12-12 17:34:19 +0300

    create Musician.cs

--- W11_CrazyMusicians/Models/Musician.cs
+++ W11_CrazyMusicians/Models/Musician.cs
@@ -1,6 +1,19 @@
-﻿namespace W11_CrazyMusicians.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace W11_CrazyMusicians.Models;
 
 public class Musician
 {
+    public int Id { get; set; }
     
+    [Required(ErrorMessage = "Name is required")]
+    [StringLength(50, MinimumLength = 2, ErrorMessage = "Name must be between 2 and 50 characters")]
+    public string Name { get; set; }
+    
+    [Required(ErrorMessage = "Occupation is required")]
+    [StringLength(50, MinimumLength = 4, ErrorMessage = "Occupation must be between 4 and 50 characters")]
+    public string Occupation { get; set; }
+    
+    [Required(ErrorMessage = "Fun fact is required")]
+    public string FunnyFact { get; set; }
 }
